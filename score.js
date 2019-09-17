04a2b3e7b07214cd160d9dc640bed01a259ede7b
HariharanSrikrishnan <harisshd.4535@gmail.com>
2019-09-17 20:55:56 +0530

    Ädded workflow

--- score.js
+++ score.js
@@ -11,33 +11,52 @@ gameOver = false;
 
 
 p1Button.addEventListener("click", function(){
-if(!gameOver){
-p1Score++;
-if(p1Score === winningScore){
-p1Display.classList.add("winner");
-gameOver = true;
-}
-p1Display.textContent = p1Score;
- }
+	if(!gameOver){
+		p1Score++;
+		if(p1Score === winningScore){
+			p1Display.classList.add("winner");
+			gameOver = true;
+		}
+		p1Display.textContent = p1Score;
+	}
 });
+
+
 
 p2Button.addEventListener("click", function(){
-if(!gameOver){
-p2Score++;
-if(p2Score === winningScore){
-p2Display.classList.add("winner");
-gameOver = true;
-}
-p2Display.textContent = p2Score;
- }
+	if(!gameOver){
+		p2Score++;
+		if(p2Score === winningScore){
+			p2Display.classList.add("winner");
+			gameOver = true;
+		}
+		p2Display.textContent = p2Score;
+	}
 });
+
+
 
 resetButton.addEventListener("click", function(){
-p1Display.textContent = 0;
-p2Display.textContent = 0;
+	reset();
+	
 });
 
+
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+
+}
+
+
+
 numInput.addEventListener("change", function(){
-winningScoreDisplay.textContent = this.value;
-winningScore = Number(this.value);
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
 });
